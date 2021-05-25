9912f892e5d777747a343c234b56ec2ed60b66a6
tuncerrstm <tuncerrstm@gmail.com>
2021-05-25 05:32:20 +0300

    1.Görev
    Projenin 1.Bölümü Tamamlandı.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        [Key]
+        public int ContentID { get; set; }
+
+        [StringLength(1000)]
+        public string ContenValue { get; set; }
+
+        public DateTime ContentDate { get; set; }
+
+        public int TitleID { get; set; }
+        public virtual Title Title { get; set; }
+
+        public int? WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrete/Title.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Title
+    {
+        [Key]
+        public int TitleID { get; set; }
+
+        [StringLength(50)]
+        public string TitleName { get; set; }
+
+        public DateTime TitleDate { get; set; }
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        public ICollection<Content> Contents { get; set; }
+    }
+}
